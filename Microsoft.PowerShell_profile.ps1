1596fc87e7a6d6c33a09c33eb6765b835ea63fb9
kochser <s225169@student.dtu.dk>
2024-04-10 22:56:11 +0200

    Updated file

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,13 +1,19 @@
 Invoke-Expression (&starship init powershell)
+
 Import-Module -Name Terminal-Icons
+
 Import-Module posh-git
 $GitPromptSettings.EnableFileStatus = $false
-
-New-Alias -Name or -Value rider64.exe
-New-Alias -Name ss -Value Select-String
 
 Function Open-Explorer 
 { 
    explorer.exe (Get-Location).Path 
 }
+
 New-Alias -Name oexp -Value Open-Explorer
+New-Alias -Name g -Value git
+New-Alias -Name or -Value rider64.exe
+New-Alias -Name ss -Value Select-String
+#
+# The following is specific to the branch
+#
